a40e736f762213d93e27f0185723e5a1169f0525
tomasz <tomasztargo@gmail.com>
2017-06-24 13:01:54 +0200

    poprawiona funkcja bez if

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,6 @@
 var infoButtons = document.getElementsByClassName('button');
 	
 console.log(infoButtons.length);
-for (var i = 0; i<=infoButtons.length; i++){
-	if (i <= infoButtons.length -1){
+for (var i = 0; i<infoButtons.length; i++){
 		console.log(infoButtons[i].innerText);
-	} 
 }
